70a850f7e7b99323b101bb7c77bd761cdc6fa0a4
andre-ve <andre.veski@gmail.com>
2017-12-09 12:12:24 +0200

    * reduced experiment sweep

--- ssp_experiment_4.py
+++ ssp_experiment_4.py
@@ -7,10 +7,10 @@ exp_logger = Logger(sys.argv[1])
 initial_bots_values = [10, 20, 50, 100]
 spawn_rate_values = [1, 2, 3, 5, 8, 10, 15, 20]
 o_penalty_values = range(10)
-bot_r_values = range(6)
+bot_r_values = range(5)
 
 i = 0
 for initial_bots_e, spawn_rate_e, o_penalty_e, bot_r_e in product(initial_bots_values, spawn_rate_values, o_penalty_values, bot_r_values):
-    exp = Experiment(exp_id=i, logger=exp_logger, grid_size = 100, initial_bots = initial_bots_e, spawn_rate = spawn_rate_e, iterations = 10000, o_penalty = o_penalty_e, bot_r = bot_r_e)
+    exp = Experiment(exp_id=i, logger=exp_logger, grid_size = 100, initial_bots = initial_bots_e, spawn_rate = spawn_rate_e, iterations = 1000, o_penalty = o_penalty_e, bot_r = bot_r_e)
     i += 1
     exp.run()
